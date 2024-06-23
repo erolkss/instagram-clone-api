d0674a8ba13cf0f8ca8e577516039928c4c037e7
Lucas Lima <lucaserolima@gmail.com>
2024-06-23 11:59:58 -0400

    First Commit - Create a project

---
+++ instagram_web_api/src/main/java/br/com/ero/instagram_web_api/InstagramWebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.instagram_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramWebApiApplication.class, args);
+	}
+
+}
