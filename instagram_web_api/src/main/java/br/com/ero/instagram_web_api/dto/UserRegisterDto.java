5c581463f1455ef8307905e4fb83df157ae9ee71
Lucas Lima <lucaserolima@gmail.com>
2024-06-25 20:56:10 -0400

    feat: Added some validations and removed the email attribute from update user

--- instagram_web_api/src/main/java/br/com/ero/instagram_web_api/dto/UserRegisterDto.java
+++ instagram_web_api/src/main/java/br/com/ero/instagram_web_api/dto/UserRegisterDto.java
@@ -1,6 +1,8 @@
 package br.com.ero.instagram_web_api.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +17,10 @@ public class UserRegisterDto {
     @NotBlank(message = "Full Name is mandatory")
     private String name;
     @NotBlank(message = "E-mail is mandatory")
+    @Email(regexp = "^[a-z0-9.+-]+@[a-z--9.-]+\\.[a-z]{2,}$", message = "Invalid email format")
     private String email;
     @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, max = 30)
     private String password;
 
 }
